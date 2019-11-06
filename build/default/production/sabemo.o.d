3b2b348faba9c90af09b26202cf4a9c16833b637
dBarquet <diego.barquet@gmail.com>
2019-11-06 16:41:23 -0200

    sabemo commit

---
+++ build/default/production/sabemo.o.d
@@ -0,0 +1,2 @@
+build/default/production/sabemo.o:  \
+sabemo.c 
---
+++ sabemo.c
@@ -0,0 +1,17 @@
+/* 
+ * File:   sabemo.c
+ * Author: Diego
+ *
+ * Created on 6 de noviembre de 2019, 12:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int i;
+
+/*
+ * 
+ */
+
+
