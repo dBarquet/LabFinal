c36e3df96a816862ac1fa946c86b991b05782acf
dBarquet <55771625+dBarquet@users.noreply.github.com>
2019-11-06 13:03:47 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Diego
+ *
+ * Created on 6 de noviembre de 2019, 11:42 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
