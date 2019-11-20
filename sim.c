5133d3a60236409af28e4f8da73fb568823f6772
dBarquet <diego.barquet@gmail.com>
2019-11-19 22:11:04 -0200

    ultimas de sim

--- sim.c
+++ sim.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <xc.h>
 #include <sys/attribs.h>     //MACROS ISRs
 #include "mcc_generated_files/mcc.h"
@@ -34,30 +35,55 @@ switch(SIMstate) {
 	case (2):
         if (UART1_IsRxReady(void)) {
             UART1_WriteBuffer ( "A" ,  strlen ("A"));
+			SIMstate=3;
+        }
+    break;
         
+    case (3):
+        if ( delayMs( 4000 ) == true) {
+            UART1_WriteBuffer ( "AT\r" ,  strlen ("AT\r"));
+            SIMstate=4;
+        }
+    break;            
+    
+    case (4):
+        if (UART1_ReadBuffer ( "\r\nOK\r\n" , strlen("\r\nOK\r\n") ) == 0){
+            SIMstate=5;
+            }
+            else
+                if ( delayMs( 3000 ) == true) {
+                 SIMstate=3;
+                }
+        break;
+        
+
 	
     
     
     
-    case (2):
-	if (necesita sim) {
-		Buffer='Ingrese SIM';
-		putsUSBUSART( Buffer );
-        CDCTxService( );
-		SIMstate=3;
-	}
-	else SIMstate=4;
-	break
+    case (5):
+        UART1_WriteBuffer ( "AT+CPIN?\r" ,  strlen ("AT+CPIN?\r"));
+        
+        
+        if (necesita sim) {
+		//Buffer='Ingrese SIM';
+		//putsUSBUSART( Buffer );
+        //CDCTxService( );
+		memccpy(Buffer, SIM, 4);
+            SIMstate=6;
+/*	}
+	else SIMstate=7;
+	break;
 	
-	case (3):
+	case (6):
 		if(getsUSBUSART( Buffer, length )!=0 ){
 				memccpy(Buffer, SIM, 4);
 			meter el sim en modulo
-			SIMstate=4;
-		}
+			SIMstate=7;
+*/		}
 	break
 	
-	case (4):
+	case (6):
 	GPS
 }}
 }
